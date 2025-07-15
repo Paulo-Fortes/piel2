db169d630599dbd4fe98505c73ac45ef401c81ce
Paulo-Fortes <paulorobertoroco@gmail.com>
2025-07-15 16:39:29 -0300

    Implementación de paginación, header, botón favorito (corazón) funcionando y filtrando de manera funcional.

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,52 +1,115 @@
-import {Box, Image, Text, Badge, Flex, IconButton, Skeleton /* , Icon */} from '@chakra-ui/react';
+import {Box, Image, Text, Badge, Flex, IconButton, Skeleton } from '@chakra-ui/react';
 import {BiExpand} from 'react-icons/bi';
-import React from 'react'
-/* import { scale } from 'framer-motion'; */
-
+import React from 'react';
+import {addToFavorites, removeFromFavorites} from '../redux/actions/productActions';
+import {useSelector, useDispatch} from 'react-redux';
+import {MdOutlineFavorite, MdOutlineFavoriteBorder} from 'react-icons/md';
+import '@fontsource-variable/montserrat';
 
 
 const ProductCard = ({product, loading}) => {
-    return <Skeleton isLoaded={!loading} _hover={{size: 1.5}}>
-        <Box
-        _hover={{transform: 'scale(1.1)', transitionDuration: '0.5s'}}
-        borderWidth='0px'
-        overflow='hidden'
-        backgroundColor='orange.50'
-        p='6'
-        rounded='3xl'
-        shadow='md'
-        borderColor='gray.300'
-        /* boxShadow='inner' */        
-        >
-            <Image src={product.images[0]} fallbackSrc='http:/via.placeholder.com/150' alt={product.name} height='200px' rounded='xl' shadow='md'/>
+    const dispatch = useDispatch();
+    const {favorites} = useSelector((state) => state.product);
+
+
+
+    return (
+        <Skeleton isLoaded={!loading} _hover={{size: 1.5}}>
+            <Box
+                _hover={{transform: 'scale(1.1)', transitionDuration: '0.3s'}}
+                borderWidth='1px'
+                overflow='hidden'
+                /* backgroundColor='orange.50' */
+                backgroundColor='#FFF4E5'                
+                p='6'
+                rounded='3xl'
+                shadow='md'
+                borderColor='gray.300'>
+                    <Image 
+                        src={product.images[0]} 
+                        fallbackSrc='https://via.placeholder.com/150' 
+                        alt={product.name} 
+                        height='200px' 
+                        rounded='xl' 
+                        shadow='md'
+                    />
             <br></br>
                 {product.stock === 0 ? (
                 <Badge colorScheme='red'>Agotado</Badge>
-            ) : product.stock < 5 ? (
-                <Badge colorScheme='yellow'> En stock {product.stock} itens</Badge>
-            ) : (
-                <Badge colorScheme='green' >Hay stock</Badge>
+                ) : product.stock < 5 ? (
+                <Badge colorScheme='yellow'> En stock {product.stock} unid.</Badge>
+                ) : (
+                <Badge colorScheme='green'>Hay stock</Badge>
                 )}
                 {product.productIsNew && (
                     <Badge ml='2' colorScheme='purple'>
                         Nuevo
                     </Badge>
                 )}
-                <Text noOfLines={1} fontSize='xl' fontWeight='semibold' mt='2'color='gray.600'>
+                <Text noOfLines={1} fontSize='xl' fontWeight='semibold' mt='2' color='#96634E'>
                     {product.name}
                 </Text>
-                <Text noOfLines={1} fontSize='md' color='gray.500'>
+                <Text noOfLines={1} fontSize='md' color='#604113'>
                     {product.subtitle}
                 </Text>
                 <Flex justify='space-between' alignItems='center' mt='2'>
-                    <Badge colorScheme='gray'>{product.category}</Badge>
-                    <Text fontSize='xl' fontWeight='semibold' shadow='inner' rounded='xl' p='2' color='teal.400' borderColor='gray.200'>
-                        AR${' '}{product.price}
-                    </Text>
+                    <Badge 
+                        backgroundColor='#FFF4E5'
+                        rounded='xl'
+                        p='2'
+                        fontWeight='normal'
+                        fontSize='sm'
+                        color='#96634E'
+                        boxShadow='inner'>
+                            {product.category}
+                    </Badge>
+                    <Badge 
+                        backgroundColor='#FFF4E5'
+                        rounded='xl' 
+                        p='2'
+                        boxShadow='sm'>
+                            <Text 
+                            fontSize='md'                        
+                            fontWeight='bold'
+                            color='#96634E'
+                            >
+                            AR${' '}{product.price}
+                        </Text>
+                    </Badge>                
                 </Flex>
-                <IconButton icon={<BiExpand size='20'/>} colorScheme='yellow' size='sm' shadow='md' />
-        </Box>
-    </Skeleton>    
+                <Flex justify='space-between' mt='2'>
+                    {favorites.includes(product._id) ? (
+                        <IconButton
+                            icon={<MdOutlineFavorite size='20px' />}
+                            colorScheme='red'
+                            boxShadow='inner'
+                            size='md'
+                            isRound={true}
+                            onClick={() => dispatch(removeFromFavorites(product._id))}
+                        />
+                    ):(
+                        <IconButton
+                            icon={<MdOutlineFavoriteBorder size='20px' />}
+                            color='#886128'
+                            backgroundColor='#FFF4E5'
+                            size='md'
+                            boxShadow='inner'
+                            isRound={true}
+                            onClick={() => dispatch(addToFavorites(product._id))}
+                        />                        
+                    )} 
+                    <IconButton 
+                        icon={<BiExpand size='20'/>} 
+                        /*colorScheme='yellow'*/ 
+                        color='#886128'
+                        backgroundColor='#FFF4E5' 
+                        size='md' 
+                        isRound={true} 
+                        shadow='inner' />
+                </Flex>                
+            </Box>
+        </Skeleton>
+    );  
 };
 
 export default ProductCard;
--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,22 +1,28 @@
-import { Box, Wrap, WrapItem, Center } from '@chakra-ui/react';
+import { Box, Button, Center, IconButton, Wrap, WrapItem } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import ProductCard from '../components/ProductCard';
-/* import axios from 'axios'; */
-import { useEffect, /* useState  */} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { getProducts } from '../redux/actions/productActions';
+import { MdKeyboardDoubleArrowLeft, MdKeyboardDoubleArrowRight } from "react-icons/md";
+
+
 
 const ProductsScreen = () => {
 	const dispatch = useDispatch();
-	const { loading, /* error, */ products/* , pagination  */} = useSelector((state) => state.product);
+	const { loading, /* error, */ products, pagination, favoritesToggled } = useSelector((state) => state.product);
 
 	useEffect(() => {
-		dispatch(getProducts());
+		dispatch(getProducts(1));
 	}, [dispatch]);
+
+	const paginationButtonClick = (page) => {
+		dispatch(getProducts(page));
+	};
 
 	return (
 		<>
-			{products.length > 1 && (
-				<Box>
+			{products.length >= 1 && (
+				<Box mt='10'>
 					<Wrap spacing='30px' justify='center' minHeight='80vh' mx={{ base: '12', md: '20', lg: '32' }}>
 						{products.map((product) => (
 							<WrapItem key={product._id}>
@@ -26,6 +32,26 @@ const ProductsScreen = () => {
 							</WrapItem>
 						))}
 					</Wrap>
+					{!favoritesToggled && (
+						<Wrap spacing='10px' justify='center' p='5' mt='4' isRound={true}>
+							<IconButton colorScheme='gray' isRound={true} onClick={() => paginationButtonClick(1)}>
+								<MdKeyboardDoubleArrowLeft/>
+							</IconButton>
+							{Array.from(Array(pagination.totalPages), (e, i) => {
+								return (
+									<Button
+										colorScheme={pagination.currentPage === i + 1 ? 'gray' : 'gray'}
+										key={i}
+										onClick={() => paginationButtonClick(i + 1)}>
+										{i + 1}
+									</Button>
+								);
+							})}
+							<IconButton colorScheme='gray' isRound={true} onClick={() => paginationButtonClick(pagination.totalPages)}>
+								<MdKeyboardDoubleArrowRight/>
+							</IconButton>
+						</Wrap>
+					)}
 				</Box>
 			)}
 		</>
